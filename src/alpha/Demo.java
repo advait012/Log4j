7ab09134b2cfa6cfc0780828374c71ab6944cc73
advait012 <42748081+advait012@users.noreply.github.com>
2019-07-24 21:56:34 +0530

    first commit

---
+++ src/alpha/Demo.java
@@ -0,0 +1,23 @@
+package alpha;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class Demo {
+	private static Logger log=	 LogManager.getLogger(Demo.class.getName());
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	try {
+		
+	}
+	catch(Exception e)
+	{
+		log.error(e.getMessage());
+	}
+	
+
+	}
+
+}
